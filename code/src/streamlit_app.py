4a46575720cd266e9689b9e5624ed89d0882fd7e
Lesly León <79551230+MariaLesly@users.noreply.github.com>
2025-10-03 01:59:47 -0500

    Update streamlit_app.py

--- code/src/streamlit_app.py
+++ code/src/streamlit_app.py
@@ -105,14 +105,14 @@ with tab1:
     if centros is not None:
         st.subheader("🏘️ Directorio Nacional de Centros Poblados")
         #st.dataframe(centros.head(10))
-        "st.write(f"**Total de registros:** {len(centros)} centros poblados")
-	st.write(f"**Total de registros:** 100 centros poblados")
+        #st.write(f"**Total de registros:** {len(centros)} centros poblados")
+		st.write(f"**Total de registros:** 100 centros poblados")
 
     if distritos is not None:
         st.subheader("🗺️ Límites Distritales a Nivel Nacional")
         #st.dataframe(distritos.head(10))
         #st.write(f"**Total de registros:** {len(distritos)} distritos")
-	st.write(f"**Total de registros:** 100 distritos")
+		st.write(f"**Total de registros:** 100 distritos")
 
 # TAB 2 - MAPAS ESTÁTICOS
 with tab2:
